43610e08328c2b6f22e28f5b4d1f568491623c0a
Веприеов <98660333+Vepricov@users.noreply.github.com>
2022-01-29 23:00:06 +0300

    Add files via upload

---
+++ 3 lab/drop table.sql
@@ -0,0 +1,16 @@
+﻿use TV
+
+drop table REPORT_MAKERS
+drop table BROADCAST
+drop table REPORT
+drop table [CONTRACT]
+drop table JOB
+drop table EMPLOYEE
+drop table DEPARTMENT
+drop table CHANNEL
+drop table [EVENT]
+drop table CITY
+drop table [TYPE]
+drop table QUALITY
+drop table DANGER
+drop table COUNTRY
