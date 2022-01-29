34213962786d07d6e8774ecb1a5489b8dd103c5d
Веприеов <98660333+Vepricov@users.noreply.github.com>
2022-01-29 22:44:50 +0300

    Update 3.sql

--- 4 lab/3.sql
+++ 4 lab/3.sql
@@ -1,12 +1,12 @@
 use TV
---Корреспондент, страна, среднее время репортажа, количество репортажей, средняя степень опасности репортажа.
+--РљРѕСЂСЂРµСЃРїРѕРЅРґРµРЅС‚, СЃС‚СЂР°РЅР°, СЃСЂРµРґРЅРµРµ РІСЂРµРјСЏ СЂРµРїРѕСЂС‚Р°Р¶Р°, РєРѕР»РёС‡РµСЃС‚РІРѕ СЂРµРїРѕСЂС‚Р°Р¶РµР№, СЃСЂРµРґРЅСЏСЏ СЃС‚РµРїРµРЅСЊ РѕРїР°СЃРЅРѕСЃС‚Рё СЂРµРїРѕСЂС‚Р°Р¶Р°.
 
-select EMPLOYEE.first_name, EMPLOYEE.last_name, COUNTRY.[name], avg(DATEDIFF(MINUTE, '0:0:0', REPORT.duration) * 1.0) as 'среднее время репортажа(мин)',
-count(REPORT_MAKERS.contract_id) as 'количество репортажей', avg(DANGER.[level] * 1.0) as 'средняя степень опасности репортажа'
+select EMPLOYEE.first_name, EMPLOYEE.last_name, COUNTRY.[name], avg(DATEDIFF(MINUTE, '0:0:0', REPORT.duration) * 1.0) as 'СЃСЂРµРґРЅРµРµ РІСЂРµРјСЏ СЂРµРїРѕСЂС‚Р°Р¶Р°(РјРёРЅ)',
+count(REPORT_MAKERS.contract_id) as 'РєРѕР»РёС‡РµСЃС‚РІРѕ СЂРµРїРѕСЂС‚Р°Р¶РµР№', avg(DANGER.[level] * 1.0) as 'СЃСЂРµРґРЅСЏСЏ СЃС‚РµРїРµРЅСЊ РѕРїР°СЃРЅРѕСЃС‚Рё СЂРµРїРѕСЂС‚Р°Р¶Р°'
 from EMPLOYEE
 join COUNTRY on COUNTRY.country_id = EMPLOYEE.country_id
 join [CONTRACT] on [CONTRACT].employee_id = EMPLOYEE.employee_id
-join JOB on JOB.job_id = [CONTRACT].job_id and JOB.[function] = 'Репортёр'
+join JOB on JOB.job_id = [CONTRACT].job_id and JOB.[function] = 'Р РµРїРѕСЂС‚С‘СЂ'
 join REPORT_MAKERS on REPORT_MAKERS.contract_id = [CONTRACT].contract_id
 join REPORT on REPORT_MAKERS.report_id = REPORT.report_id
 join [EVENT] on [EVENT].event_id = REPORT.event_id
